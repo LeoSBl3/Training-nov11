997bf8f2848b748841afd3a3f1cb04c49750f186
LeoSBl3 <leo.shawkat@gmail.com>
2022-11-17 15:43:27 +0100

    commit test 3

--- src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
@@ -15,8 +15,6 @@ import java.util.Map;
 public class HelloWorldHandler implements RequestHandler<Object, Object> {
 
     public Object handleRequest(final Object input, final Context context) {
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        return new GatewayResponse(new JSONObject().put("Output", "Hello World!").toString(), headers, 200);
+
     }
 }
